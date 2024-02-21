0a8de2f889d92ca6b1240fc77b5de3c73df3a683
aaljo222 <125717229+aaljo222@users.noreply.github.com>
2024-02-21 22:53:05 +0900

    Update Data.js

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -191,7 +191,7 @@ export const Data = {
       },
       {
         name: "기업 프로젝트기반 빅데이터를 활용한 백앤드 개발자 양성",
-        company: "천호 아트 아카데미",
+        company: "천호 국제 아트 아카데미",
         period: "Jun. 2023 - Jan 2024",
         description: [
           "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
