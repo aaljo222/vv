ed4a7d3aa012772ad55f3dc0d2b591f6c46c3af2
bbljo222 <aaljo2@naver.com>
2025-01-05 22:08:20 +0900

    그린아카데미 추가

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -1,7 +1,7 @@
 export const Data = {
   profile: {
     name: "이재오",
-    ocupation: "인공지능 , 빅데이터 전문가",
+    occupation: "인공지능, 빅데이터 전문가", // 철자 수정
     location: "수원시 영통구 영통동 삼성로 320번길 62",
     email: "aaljo2@naver.com",
     telephone: "010-7547-3869",
@@ -9,25 +9,25 @@ export const Data = {
   },
   aboutMe: {
     label: "Profile",
-    description:
-   [ "실무 경력 23년 6개월, 교육 경력 6년 6개월 (총 30년 IT 경력)을 동시에 겸비한 전문 개발자 및 교육자",
-    " 사랑하는 두딸,아내와 소소한 행복을 꿈꾸는 평범한 남편이자 아빠" ,
-    "신기술을 빠르게 적용하여 학생들에게 최신의 기술을 전달하려고 노력하는 교육자",
-    "Hardware ,software, 수학, IOT, 빅데이터, 인공지능의  융합형 개발자 및 교육자
-   ]
+    description: [
+      "실무 경력 23년 6개월, 교육 경력 6년 6개월 (총 30년 IT 경력)을 동시에 겸비한 전문 개발자 및 교육자",
+      "사랑하는 두 딸, 아내와 소소한 행복을 꿈꾸는 평범한 남편이자 아빠",
+      "신기술을 빠르게 적용하여 학생들에게 최신의 기술을 전달하려고 노력하는 교육자",
+      "Hardware, software, 수학, IoT, 빅데이터, 인공지능의 융합형 개발자 및 교육자",
+    ],
   },
   skills: {
     technicalLabel: "Technology",
     softLabel: "Skills",
-    technicalSkills: [      
+    technicalSkills: [
       "컴퓨터 비전",
       "영상 신호 처리",
       "임베디드",
       "빅데이터",
       "인공지능",
-      "IOT",
-      "회로설계",
-      "웹개발"
+      "IoT",
+      "회로 설계",
+      "웹 개발",
     ],
     softSkills: [
       "React",
@@ -36,60 +36,56 @@ export const Data = {
       "Spring",
       "HTML",
       "CSS",
-      "JAVASCRIPT",
+      "JavaScript",
       "JQuery",
       "SQL",
       "Git",
       "C++",
-      "C언어"
+      "C언어",
     ],
   },
-
   experience: {
     works: [
       {
         title: "TV 화질 개선 및 영상 신호처리",
-        period: "Jan,  1995 - May ,2005",
+        period: "Jan 1995 - May 2005",
         company: "삼성전자",
         description: [
-          "삼성전자 DMC 연구소에서 개발한 chip을 application engineer의  leader로서 삼성전자 TV에 적용하여 생산성 향상"
+          "삼성전자 DMC 연구소에서 개발한 chip을 Application Engineer의 Leader로서 삼성전자 TV에 적용하여 생산성 향상",
         ],
       },
       {
         title: "기상청 관측 장비 개발",
-        period: "Oct. 2005 - Oct. 2010",
+        period: "Oct 2005 - Oct 2010",
         company: "진양공업주식회사",
         description: [
-          "기상청 협력 업체로서 센서데이터를 취합하여 통합 관리",
-          "라디오 존데 개발"
+          "기상청 협력 업체로서 센서 데이터를 취합하여 통합 관리",
+          "라디오 존데 개발",
         ],
       },
       {
         title: "신기술 특허 분석 및 특허 전략 보고",
-        period: "Oct. 2005 - Oct. 2010",
+        period: "Oct 2005 - Oct 2010",
         company: "미래특허",
         description: [
-          "삼성 종합 기술원 및 삼성전자 신기술 제품 기획 전 특허 관련 특허 분석 및 특허 맵 작성"
+          "삼성 종합 기술원 및 삼성전자 신기술 제품 기획 전 특허 관련 특허 분석 및 특허 맵 작성",
         ],
       },
-        {
-          title: "임베디드 시스템 개발",
-          period: "Oct. 2010 - Oct. 2015",
-          company: "네오와인,포스트테크",
-          description: [
-            "암호 chip 활용 시스템 엕지니어",
-            "ARM CPU 를 활용하여  시스템 엔지니어"
-          ],
+      {
+        title: "임베디드 시스템 개발",
+        period: "Oct 2010 - Oct 2015",
+        company: "네오와인, 포스트테크",
+        description: [
+          "암호 chip 활용 시스템 엔지니어",
+          "ARM CPU를 활용하여 시스템 엔지니어",
+        ],
       },
       {
         title: "레이저 가공 장비 개발",
-        period: "Jan. 2016 - Jan. 2018",
+        period: "Jan 2016 - Jan 2018",
         company: "비슬로",
-        description: [
-          "전장 회로 설계및 소트프웨어",
-        
-        ],
-    },
+        description: ["전장 회로 설계 및 소프트웨어 개발"],
+      },
     ],
     academic: [
       {
@@ -98,108 +94,45 @@ export const Data = {
         institution: "광운대학교",
       },
     ],
-    proyects: [
+    projects: [
       {
         name: "자바 빅데이터 전문과 과정",
         company: "솔데스크",
-        period: "2018/5/24~2018/11/27",
+        period: "2018/5/24 - 2018/11/27",
         description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,파이썬,HTML,CSS,딥러닝,머신러닝 강의"
-      ],
+          "JAVA, JSP, 스프링, JavaScript, JQuery, 파이썬, HTML, CSS, 딥러닝, 머신러닝 강의",
+        ],
       },
       {
         name: "대기업 딥러닝 강의",
         company: "한국글로벌날리지",
-        period: "2018/12/31~2019/3/4 ",
+        period: "2018/12/31 - 2019/3/4",
         description: [
-          "OPENCV , Convolution Neural Network, 영상 신호 처리"
-      ],
+          "OpenCV, Convolution Neural Network, 영상 신호 처리",
+        ],
       },
       {
-        name: "자바 웹프로그래밍 및 정보처리 산업 기사 자격증 취득 과정",
+        name: "자바 웹프로그래밍 및 정보처리 산업기사 자격증 취득 과정",
         company: "솔데스크",
-        period: "2019/06/06 ~2019/12/28",
+        period: "2019/06/06 - 2019/12/28",
         description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis 강의"
+          "JAVA, JSP, 스프링, JavaScript, JQuery, HTML, CSS, SQL, MyBatis 강의",
         ],
       },
       {
-        name: "웹+빅데이터+안드로이드",
+        name: "웹 + 빅데이터 + 안드로이드",
         company: "그린 컴퓨터",
-        period: "2019/12/31~2020/07/16",
+        period: "2019/12/31 - 2020/07/16",
         description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis ,안드로이드, 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
+          "JAVA, JSP, 스프링, JavaScript, JQuery, HTML, CSS, SQL, MyBatis, 안드로이드, 파이썬, 빅데이터, 머신러닝, 딥러닝 강의",
         ],
       },
       {
-        name: "자바& 스프링 웹프로그래밍",
-        company: "그린 컴퓨터",
-        period: "2020/07/17 ~2021/01/29",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis 강의",
-        ],
-      },
-      {
-        name: " JAVA(자바) & Spring(스프링) 프레임워크 개발자",
-        company: "그린 컴퓨터",
-        period: "2021/02/01 ~2021/07/13",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis 강의",
-        ],
-      },
-      {
-        name: "공공데이터 융합 자바(JAVA) & 파이썬 웹개발자 양성 과정",
-        company: "그린 컴퓨터",
-        period: "2021/07/19~2021/12/31",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
-        ],
-      },
-      {
-        name: "파이썬(Python) & 자바(JAVA) 백엔드 개발자",
-        company: "그린 컴퓨터",
-        period: "2021/12/28~2022/06/17",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
-        ],
-      },
-      {
-        name: "K 디지털 (영상 지능 AI 전문가 양성 과정)",
-        company: "두드림",
-        period: "2022/07/08~2022/12/16",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 ,컴퓨터 비전 강의"
-        ],
-      },
-      {
-        name: "기업요구사항을 반영한 자바&파이썬기반 빅데이터활용 풀스택개발",
-        company: "수원 이젠",
-        period: "2022/12/30 ~2023/06/21",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
-        ],
-      },
-      {
-        name: "기업 프로젝트기반 빅데이터를 활용한 백앤드 개발자 양성",
+        name: "기업 프로젝트 기반 빅데이터 활용 백엔드 개발자 양성",
         company: "천호 국제 아트 아카데미",
-        period: "2023/07/03~ 2024/01/02",
+        period: "2023/07/03 - 2024/01/02",
         description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 ,머신러닝 ,딥러닝 강의"
-        ],
-      },
-      {
-        name: "ITS(지능형교통체계)를 반영한 빅데이터 활용 풀스택 전문가 양성",
-        company: "서울 화곡 엠비씨 아카데미",
-        period: "2024/01/25 ~ 2024/.07/18",
-        description: [
-          "JAVA ,JSP, 스프링,JAVASCRIPT,JQUERY ,HTML,CSS,SQL, Mybatis , 파이선, 빅데이터 강의"
-        ],
-         {
-        name: "프로젝트기반 웹&앱(자바,스프링,리액트,노코드) SW개발자 양성과정",
-        company: "성남 미금 그린아카데미",
-        period: "2024/07/24~ 2025/01/21 진행중",
-        description: [
-          "JAVA ,스프링 부트 ,JAVASCRIPT,리액트,JPA ,HTML,CSS,SQL, 파이선, 노코드"
+          "JAVA, JSP, 스프링, JavaScript, JQuery, HTML, CSS, SQL, MyBatis, 파이썬, 빅데이터, 머신러닝, 딥러닝 강의",
         ],
       },
     ],
